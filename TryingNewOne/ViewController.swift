b6f6e062ef935b65b2027a2a20c1232b6a16a85c
Fuad Hasan <fuad.hasan2015@gmail.com>
2019-09-03 19:37:43 +0600

    Initial Commit

---
+++ TryingNewOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TryingNewOne
+//
+//  Created by Fuad Hasan on 3/9/19.
+//  Copyright © 2019 Fuad Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
